da9bb49aa728d0edd10ec8241823548381d53306
Ivan Biedermann <neeodev@gmail.com>
2021-01-06 13:59:18 +0100

    Remove useless thinks :fire:

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Hello world</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
